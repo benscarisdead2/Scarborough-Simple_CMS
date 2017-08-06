858d7cc85d38f356ec1b4f0b987466f59f071649
ben scarborough <benscarborough@scarflix.com>
2017-08-06 02:51:08 -0400

    Added migrations

--- db/migrate/20170806050959_create_users.rb
+++ db/migrate/20170806050959_create_users.rb
@@ -2,14 +2,14 @@ class CreateUsers < ActiveRecord::Migration[5.1]
 
   def up
     create_table :users do |t|
-      t.column "first_name", :string :limit => 25
+      t.column "first_name", :string, :limit => 25
       t.string "last_name", :limit => 50
       t.string "email", :default => '', :null => false
       t.string "password", :limit => 40
 
-      t.datetime "created_at"
-      t.datetime "updated_at"
-      
+      # t.datetime "created_at"
+      # t.datetime "updated_at"
+
       t.timestamps
     end
   end
